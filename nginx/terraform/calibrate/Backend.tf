99f4534652d4abb81f227ceb89da420ced068e45
Crucibl <93767513+Crucibl@users.noreply.github.com>
2022-02-04 12:41:49 -0800

    Delete nginx directory

--- nginx/terraform/calibrate/Backend.tf
+++
@@ -1,5 +0,0 @@
-terraform {
-  backend "local" {
-    path = ".terraform/terraform.tfstate"
-  }
-}
--- nginx/terraform/calibrate/Data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "local" {
-  backend = "local"
-
-  config = {
-    path = "${path.module}/.terraform/terraform.tfstate"
-  }
-}
-
-data "aws_caller_identity" "current" {}
